7ca4cbc4d238ec6af95f3d63c5116fd00750858a
czyt <czytcn@gmail.com>
2025-07-25 07:33:40 +0800

    first commit

---
+++ dist/_next/static/KmJ283VHZk62WB52Nk2P_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/6c86d9ce-7b0b96c786a66e37.js","static/chunks/411-7aaae661edde9a0c.js","static/chunks/pages/index-d8c552cb118ec372.js"],"/_error":["static/chunks/pages/_error-0ea80bb8394db041.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
